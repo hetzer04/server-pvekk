ecdc262f8780de68ba28e4e98dfd1a7a6398849e
Hetzer <alex_fox1704@mail.ru>
2024-10-10 07:36:43 +0500

    Changes to be committed:

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js
-const serverless = require('serverless-http');
-const expressApp = require('../src/app'); // Импортируйте ваше Express-приложение
-
-module.exports = serverless(expressApp);
